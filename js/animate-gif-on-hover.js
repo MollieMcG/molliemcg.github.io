7e9c6154fd15c98655920f233c548d9152fe7ff1
MollieMcG <molliemcgalliard@gmail.com>
2024-12-18 14:09:47 -0500

    fix: animate gif on hover on case study pages

---
+++ js/animate-gif-on-hover.js
@@ -0,0 +1,76 @@
+$(function() {
+    $("#beforeHome").hover(
+        function() {
+            $(this).attr("src", "../images/case-study-2/PAWS_Home.gif");
+        },
+        function() {
+            $(this).attr("src", "../images/case-study-2/PAWS_Home_static.png");
+        }                         
+    );                  
+});
+
+$(function() {
+    $("#beforeAdoption").hover(
+        function() {
+            $(this).attr("src", "../images/case-study-2/PAWS_Adoption.gif");
+        },
+        function() {
+            $(this).attr("src", "../images/case-study-2/PAWS_Adoption_static.png");
+        }                         
+    );                  
+});
+
+$(function() {
+    $("#beforeDonation").hover(
+        function() {
+            $(this).attr("src", "../images/case-study-2/PAWS_Donation.gif");
+        },
+        function() {
+            $(this).attr("src", "../images/case-study-2/PAWS_Donation_static.png");
+        }                         
+    );                  
+});
+
+$(function() {
+    $("#afterHome").hover(
+        function() {
+            $(this).attr("src", "../images/case-study-2/PAWS_After_Home.gif");
+        },
+        function() {
+            $(this).attr("src", "../images/case-study-2/PAWS_After_Home_static.png");
+        }                         
+    );                  
+});
+
+$(function() {
+    $("#afterAdoption").hover(
+        function() {
+            $(this).attr("src", "../images/case-study-2/PAWS_After_Adoption.gif");
+        },
+        function() {
+            $(this).attr("src", "../images/case-study-2/PAWS_After_Adoption_static.png");
+        }                         
+    );                  
+});
+
+$(function() {
+    $("#afterDonation").hover(
+        function() {
+            $(this).attr("src", "../images/case-study-2/PAWS_After_Donation.gif");
+        },
+        function() {
+            $(this).attr("src", "../images/case-study-2/PAWS_After_Donation_static.png");
+        }                         
+    );                  
+});
+
+$(function() {
+    $("#jamsessionCover").hover(
+        function() {
+            $(this).attr("src", "../images/case-study-1-image/js-cover-gif.gif");
+        },
+        function() {
+            $(this).attr("src", "../images/case-study-1-image/js-cover-static.png");
+        }                         
+    );                  
+});
